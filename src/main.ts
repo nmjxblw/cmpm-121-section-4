e2bbf66f3cea36224dea11c2b96a2f56b8b76b89
Str4wbeary <xcai30@ucsc.edu>
2023-10-22 02:15:09 -0700

    added assets and replace rect

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,21 @@
-let info = [];
-const data = [info];
-
-const colors = [
-  "red",
-  "black",
-  "yellow",
-  "green",
-  "brown",
-  "blue",
-  "orange",
-  "pink",
-  "purple",
-  "white",
+const tiles = [
+  "assets/tile1.png",
+  "assets/tile2.png",
+  "assets/tile3.png",
+  "assets/tile4.png",
+  "assets/tile5.png",
+  "assets/tile6.png",
+  "assets/tile7.png",
 ];
-let currentColor = 0; // index of colors
+let currentTile = 0; // index of tiles
 
-const background = "#EAE9E9";
+const background = "assets/tile7.png";
 
 let click = false;
 
 const svg = document.getElementById("svg");
 const svgContainer = document.getElementById("svgContainer");
+
 
 svgContainer.appendChild(svg);
 svg.setAttribute("width", 500);
@@ -33,52 +28,65 @@ function create(elementNone) {
 function createGrid(width, height) {
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
-      const square = create("rect");
-      square.addEventListener("mouseover", function () {
+      
+      const tile = create("image");
+      
+      tile.addEventListener("mouseover", function () {
         if (click == true) {
-          if (colors[currentColor] == "white") {
-            this.setAttribute("fill", background);
-          } else {
-            this.setAttribute("fill", colors[currentColor]);
-          }
+          tile.setAttributeNS(null, "x", this.getAttribute("x"));
+          tile.setAttributeNS(null, "y", this.getAttribute("y"));
+          tile.setAttributeNS(null, "width", this.getAttribute("width"));
+          tile.setAttributeNS(null, "height", this.getAttribute("height"));
+          tile.setAttributeNS(null, 'visibility', 'visible');
+          tile.setAttributeNS('http://www.w3.org/1999/xlink', "href", tiles[currentTile])
         }
       });
-      square.addEventListener("mousedown", function () {
+      
+      tile.addEventListener("mousedown", function () {
         click = true;
-        this.setAttribute("fill", colors[currentColor]);
+        tile.setAttributeNS(null, "x", this.getAttribute("x"));
+        tile.setAttributeNS(null, "y", this.getAttribute("y"));
+        tile.setAttributeNS(null, "width", this.getAttribute("width"));
+        tile.setAttributeNS(null, "height", this.getAttribute("height"));
+        tile.setAttributeNS(null, 'visibility', 'visible');
+        tile.setAttributeNS('http://www.w3.org/1999/xlink', "href", tiles[currentTile])
       });
-      square.addEventListener("mouseup", function () {
+      
+      tile.addEventListener("mouseup", function () {
         click = false;
       });
-      square.setAttribute("x", i * 10);
-      square.setAttribute("y", j * 10);
-      square.setAttribute("width", 10);
-      square.setAttribute("height", 10);
-      square.setAttribute("fill", background);
+      
+      tile.setAttributeNS(null, "x", i*10);
+      tile.setAttributeNS(null, "y", j*10);
+      tile.setAttributeNS(null, "width", 10);
+      tile.setAttributeNS(null, "height", 10);
+      tile.setAttributeNS(null, 'visibility', 'visible');
+      tile.setAttributeNS('http://www.w3.org/1999/xlink', "href", background)
 
-      svg.appendChild(square);
+      svg?.append(tile)
     }
   }
 }
 
 function createPalette() {
-  for (let k = 0; k < colors.length; k++) {
-    const selector = create("rect");
-    selector.addEventListener("click", function () {
-      currentColor = k;
-      console.log(`SELECT${currentColor}`);
+  for (let k = 0; k < tiles.length; k++) {
+    //const selector = create("rect");
+    const img = create("image");
+    //img?.setAttribute("width", 10);
+    //img?.setAttribute("height", 10);
+    
+    img.addEventListener("click", function () {
+      currentTile = k;
+      console.log(`SELECT ${tiles[currentTile]}`);
     });
-    selector.setAttribute("x", k * 30);
-    selector.setAttribute("y", 320 + 20);
-    selector.setAttribute("width", 30);
-    selector.setAttribute("height", 30);
-    selector.setAttribute("fill", colors[k]);
-    if (colors[k] == "white") {
-      selector.setAttribute("stroke", "black");
-      //selector.innerText("Eraser");
-    }
+    img.setAttributeNS(null, "x", k * 37);
+    img.setAttributeNS(null, "y", 320 + 20);
+    img.setAttributeNS(null, "width", 35);
+    img.setAttributeNS(null, "height", 35);
+    img.setAttributeNS(null, 'visibility', 'visible');
+    img.setAttributeNS('http://www.w3.org/1999/xlink', "href", tiles[k])
 
-    svg.appendChild(selector);
+    svg.append(img)
   }
 }
 
@@ -90,16 +98,11 @@ createPalette();
 let svgText = svgContainer.innerHTML;
 var jsonString = JSON.stringify(svgText);
 
-console.log(jsonString);
+//console.log(jsonString);
 
 
 
 
 // Get the svgString from JSON
-svgText = JSON.parse(jsonString);
-console.log(svgText)
-
-//Creates a SVG Node from the string
-//svgContainer.innerHTML = svgText
-
-//Puts the SVG into the element with the id "playgroundID"
+//svgText = JSON.parse(jsonString);
+//console.log(svgText)
